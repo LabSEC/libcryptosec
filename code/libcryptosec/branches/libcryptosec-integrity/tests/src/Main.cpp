4d4477ea276827aa518d69f908d7d35ab9d4a7af
lucasmaypetry@inf.ufsc.br <lucasmaypetry@inf.ufsc.br@814833e4-271b-0410-8600-8914c28c18a0>
2014-10-15 16:43:42 +0000

    Estrutura para testes modificada e Makefile para testes criado. #38

--- code/libcryptosec/branches/libcryptosec-integrity/tests/src/Main.cpp
+++ code/libcryptosec/branches/libcryptosec-integrity/tests/src/Main.cpp
@@ -2,7 +2,7 @@
 #include "gtest.h"
 
 GTEST_API_ int main(int argc, char **argv) {
-  printf("Running main() from gtest_main.cc\n");
+  printf("Running main() from Main.cpp\n");
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
