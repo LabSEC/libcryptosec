2a78bed3deb47e0de0d5e483b5cbc5be98f86e43
pablo.montezano@grad.ufsc.br <pablo.montezano@grad.ufsc.br@814833e4-271b-0410-8600-8914c28c18a0>
2014-11-10 16:03:40 +0000

    Em retorno ao ticket #37, criado testes adicionais do Hmac: stress test, valor binario com tamanhos maiores e chave em hexa. Feito os comentários sobre as funcionalidades de cada teste.

--- code/libcryptosec/branches/libcryptosec-integrity/tests/src/HmacTest.cpp
+++ code/libcryptosec/branches/libcryptosec-integrity/tests/src/HmacTest.cpp
@@ -1,4 +1,5 @@
 #include <libcryptosec/Hmac.h>
+#include <fstream>
 #include "gtest.h"
 
 class HmacTest : public ::testing::Test {
@@ -23,8 +24,12 @@ protected:
 								"keyKEYkeyKEYkeyKEYkeyKEYkeyKEYkey"
 								"KEYkeyKEYkeyKEYkeyKEYkeyKEYkeyKEY"
 								"keyKEYkeyKEYkeyKEY");
+		unsigned char hexKey[10] = {0x12,0x12,0x12,0x12,0x12,0x12,0x12,0x12,0x12,0x12};
+		hexKey40bytes = ByteArray(&hexKey[0], 10);
+
 		ByteArray pText("plainText");
 		plainTexts = std::vector<ByteArray>(30, pText);
+
 	}
 
 	Hmac hmac;
@@ -35,12 +40,14 @@ protected:
 	ByteArray key128bytes;
 	ByteArray key129bytes;
 	ByteArray key150bytes;
+	ByteArray binaryText;
+	ByteArray hexKey40bytes;
 	std::vector<ByteArray> plainTexts;
 
 };
 
 /**
- * Geração de Hmac
+ * Gera e testa Hmac com o algoritmo sha256 e chave de 30 bytes
  */
 TEST_F(HmacTest, HmacSha256_key30bytes) {
 	hmac.init(key30bytes, MessageDigest::SHA256);
@@ -48,18 +55,27 @@ TEST_F(HmacTest, HmacSha256_key30bytes) {
 					hmac.doFinal(plainTexts[0]).toHex().c_str());
 }
 
+/**
+ * Gera e testa Hmac com o algoritmo sha256 e chave de 64 bytes
+ */
 TEST_F(HmacTest, HmacSha256_key64bytes) {
 	hmac.init(key64bytes, MessageDigest::SHA256);
 	EXPECT_STRCASEEQ("ff6d20b3d45b6c01fe8d07f155be6e94401ebb348fbaf51af8f3d4505d805306",
 					hmac.doFinal(plainTexts[0]).toHex().c_str());
 }
 
+/**
+ * Gera e testa Hmac com o algoritmo sha256 e chave de 150 bytes
+ */
 TEST_F(HmacTest, HmacSha256_key150bytes) {
 	hmac.init(key150bytes, MessageDigest::SHA256);
 	EXPECT_STRCASEEQ("9d448d43be3fde104ca6041fa33a3fd5874cabdb6d37fff8e6d6886529f222cd",
 					hmac.doFinal(plainTexts[0]).toHex().c_str());
 }
 
+/**
+ * Gera e testa Hmac com o algoritmo sha512 e chave de 30 bytes
+ */
 TEST_F(HmacTest, HmacSha512_key30bytes) {
 	hmac.init(key63bytes, MessageDigest::SHA512);
 	EXPECT_STRCASEEQ("60cf29ec2d024968219034b3843bb3f01bca73a1b03e84ab92ab4d12da9b71aa"
@@ -67,6 +83,9 @@ TEST_F(HmacTest, HmacSha512_key30bytes) {
 					hmac.doFinal(plainTexts[0]).toHex().c_str());
 }
 
+/**
+ * Gera e testa Hmac com o algoritmo sha512 e chave de 64 bytes
+ */
 TEST_F(HmacTest, HmacSha512_key64bytes) {
 	hmac.init(key128bytes, MessageDigest::SHA512);
 	EXPECT_STRCASEEQ("eeea8e1889a1ae943f8995de5eaffcefdb35a45d6fb27f8e1c14bc7d5c88cd5e"
@@ -74,6 +93,9 @@ TEST_F(HmacTest, HmacSha512_key64bytes) {
 					hmac.doFinal(plainTexts[0]).toHex().c_str());
 }
 
+/**
+ * Gera e testa Hmac com o algoritmo sha512 e chave de 150 bytes
+ */
 TEST_F(HmacTest, HmacSha512_key150bytes) {
 	hmac.init(key129bytes, MessageDigest::SHA512);
 	EXPECT_STRCASEEQ("c50e6385aa695a38309e5a6910137e52d53f82b96288de04455cf5ac4fd68567"
@@ -81,6 +103,19 @@ TEST_F(HmacTest, HmacSha512_key150bytes) {
 					hmac.doFinal(plainTexts[0]).toHex().c_str());
 }
 
+/**
+ * Testa geracao do Hmac com algoritmo sha1 e chave binaria
+ */
+TEST_F(HmacTest, HmacSha1_keyHex10bytes){
+	hmac.init(hexKey40bytes, MessageDigest::SHA1);
+
+	EXPECT_STRCASEEQ("C64E6D7457686AE0D25F2B9E5E6FF727C3FDB472",
+					hmac.doFinal("Teste").toHex().c_str());
+}
+
+/**
+ * Stress test para geracao de Hmac com o algoritmo sha512 e chave de 150 bytes
+ */
 TEST_F(HmacTest, HmacSha512Stress_key150bytes) {
 	for(int i = 0; i < 100000; i++) {
 		hmac.init(key129bytes, MessageDigest::SHA512);
@@ -90,6 +125,9 @@ TEST_F(HmacTest, HmacSha512Stress_key150bytes) {
 	}
 }
 
+/**
+ * Gera e testa Hmac com o algoritmo sha512 a partir de um vetor de chaves de 63 bytes
+ */
 TEST_F(HmacTest, HmacSha256FromVector_key63bytes) {
 	hmac.init(key63bytes, MessageDigest::SHA256);
 	hmac.update(plainTexts);
@@ -97,26 +135,62 @@ TEST_F(HmacTest, HmacSha256FromVector_key63bytes) {
 					hmac.doFinal().toHex().c_str());
 }
 
-TEST_F(HmacTest, HmacShaMd5_emptyKey) {
+/**
+ * Gera e testa Hmac com o algoritmo MD5 a partir de uma chave vazia
+ */
+TEST_F(HmacTest, HmacMd5_emptyKey) {
 	hmac.init(emptyKey, MessageDigest::MD5);
 	EXPECT_STRCASEEQ("0d371a986922b57b2247ec55f4b9bea4",
 					hmac.doFinal(plainTexts[0]).toHex().c_str());
 }
 
+/**
+ * Gera e testa Hmac com o algoritmo sha256 a partir de uma chave de 150 bytes e texto vazio
+ */
 TEST_F(HmacTest, HmacSha256FromEmptyText_key150bytes) {
 	hmac.init(key150bytes, MessageDigest::SHA256);
 	EXPECT_STRCASEEQ("0dc9c8bb5048d5219ad0f621387b1bb4a4c06e8c0fe22b24bc1dcce2a1b61677",
 					hmac.doFinal("").toHex().c_str());
 }
 
+/**
+ * Gera e testa Hmac com o algoritmo sha1 de um arquivo binario e chave de 63 bytes
+ */
+TEST_F(HmacTest, HmacSha1FromBinaryFile_key63bytes) {
+	fstream file ("./files/binaryFile.hmac2", ios::in|ios::binary|ios::ate);
+	file.seekg (0, file.end);
+	int length = file.tellg();
+	file.seekg (0, file.beg);
+
+	unsigned char * memblock = new unsigned char [length];
+
+	file.read ((char*)memblock, length);
+	file.close();
+
+	hmac.init(key63bytes, MessageDigest::SHA1);
+	ByteArray b(memblock, length);
+
+	EXPECT_STRCASEEQ("2dce9d5c64b7879fb52656953ae128a1eb2cd148",
+					hmac.doFinal(b).toHex().c_str());
+}
+
+/**
+ * Testa geracao do Hmac com texto vazio, sem chave, sem inicializacao e atraves do .doFinal()
+ */
 TEST_F(HmacTest, HmacNoInitializationAndDoFinal) {
 	EXPECT_THROW(hmac.doFinal(), InvalidStateException);
 }
 
+/**
+ * Testa geracao do Hmac com texto vazio, sem chave, sem inicializacao e atraves do .doUpdate()
+ */
 TEST_F(HmacTest, HmacNoInitializationAndUpdate) {
 	EXPECT_THROW(hmac.update(""), InvalidStateException);
 }
 
+/**
+ * Testa geracao do Hmac com algoritmo sha256, inicializando o hmac duas vezes com algoritmo e chaves diferentes
+ */
 TEST_F(HmacTest, HmacInitializationTwice) {
 	hmac.init(key30bytes, MessageDigest::SHA256);
 	hmac.init(key64bytes, MessageDigest::SHA256);
@@ -124,13 +198,21 @@ TEST_F(HmacTest, HmacInitializationTwice) {
 			hmac.doFinal(plainTexts[0]).toHex().c_str());
 }
 
+/**
+ * Testa geracao do Hmac com algoritmo sha256, com chave de 64 bytes, sem atualizar e através do .doFinal()
+ */
 TEST_F(HmacTest, HmacNoUpdateAndDoFinal) {
 	hmac.init(key64bytes, MessageDigest::SHA256);
 	EXPECT_THROW(hmac.doFinal(), InvalidStateException);
 }
 
+/**
+ * Testa geracao do Hmac com algoritmo sha256, utilizando .doFinal() duas vezes seguidas (sem inicializar a segunda vez)
+ */
 TEST_F(HmacTest, HmacDoFinalTwice) {
 	hmac.init(key30bytes, MessageDigest::SHA256);
 	hmac.doFinal("");
 	EXPECT_THROW(hmac.doFinal(), InvalidStateException);
 }
+
+
