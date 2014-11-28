1549b2cbe7825917ef45980cb7146c5490d36025
lucasperin@inf.ufsc.br <lucasperin@inf.ufsc.br@814833e4-271b-0410-8600-8914c28c18a0>
2014-11-28 14:00:37 +0000

    EllipticCurve não possui mais ponteiros para os parâmetros. Cada BigInteger já possui seus próprios ponteiros para as estruturas do OpenSSL. Simplificada a forma de criar curvas na fabrica Brainpool. Adicionado os nomes de cada curva, faltam os OIDs.

--- code/libcryptosec/trunk/src/ec/EllipticCurve.cpp
+++ code/libcryptosec/trunk/src/ec/EllipticCurve.cpp
@@ -4,64 +4,143 @@ const std::string EllipticCurve::notSpecified = "Not Specified";
 
 EllipticCurve::EllipticCurve() {
 	//Nothing to do
-	this->a = 0;
-	this->b = 0;
-	this->p = 0;
-	this->x = 0;
-	this->y = 0;
-	this->order = 0;
-	this->cofactor = 0;
 }
 
 EllipticCurve::EllipticCurve(ByteArray& encoded) {
 	//TODO
-	this->a = 0;
-	this->b = 0;
-	this->p = 0;
-	this->x = 0;
-	this->y = 0;
-	this->order = 0;
-	this->cofactor = 0;
 }
 
 EllipticCurve::EllipticCurve(std::string& encoded) {
 	//TODO
-	this->a = 0;
-	this->b = 0;
-	this->p = 0;
-	this->x = 0;
-	this->y = 0;
-	this->order = 0;
-	this->cofactor = 0;
 }
 
 EllipticCurve::~EllipticCurve() {
 }
 
 const BIGNUM* EllipticCurve::BN_a() const throw () {
-	return this->a->getBIGNUM();
+	return this->a.getBIGNUM();
 }
 
 const BIGNUM* EllipticCurve::BN_b() const throw () {
-	return this->b->getBIGNUM();
+	return this->b.getBIGNUM();
 }
 
 const BIGNUM* EllipticCurve::BN_p() const throw () {
-	return this->p->getBIGNUM();
+	return this->p.getBIGNUM();
 }
 
 const BIGNUM* EllipticCurve::BN_x() const throw () {
-	return this->x->getBIGNUM();
+	return this->x.getBIGNUM();
 }
 
 const BIGNUM* EllipticCurve::BN_y() const throw () {
-	return this->y->getBIGNUM();
+	return this->y.getBIGNUM();
 }
 
 const BIGNUM* EllipticCurve::BN_order() const throw () {
-	return this->order->getBIGNUM();
+	return this->order.getBIGNUM();
 }
 
 const BIGNUM* EllipticCurve::BN_cofactor() const throw () {
-	return this->cofactor->getBIGNUM();
+	return this->cofactor.getBIGNUM();
+}
+
+const BigInteger EllipticCurve::getA() const {
+	return a;
+}
+
+void EllipticCurve::setA(const BigInteger a) {
+	this->a = a;
+}
+
+const BigInteger EllipticCurve::getB() const {
+	return b;
+}
+
+void EllipticCurve::setB(const BigInteger b) {
+	this->b = b;
+}
+
+const BigInteger EllipticCurve::getCofactor() const {
+	return cofactor;
+}
+
+void EllipticCurve::setCofactor(const BigInteger cofactor) {
+	this->cofactor = cofactor;
+}
+
+const std::string EllipticCurve::getName() const {
+	return name;
+}
+
+void EllipticCurve::setName(const std::string name) {
+	this->name = name;
+}
+
+const std::string EllipticCurve::getOid() const {
+	return oid;
+}
+
+void EllipticCurve::setOid(const std::string oid) {
+	this->oid = oid;
+}
+
+const BigInteger EllipticCurve::getOrder() const {
+	return order;
+}
+
+void EllipticCurve::setOrder(const BigInteger order) {
+	this->order = order;
+}
+
+const BigInteger EllipticCurve::getP() const {
+	return p;
+}
+
+void EllipticCurve::setP(const BigInteger p) {
+	this->p = p;
+}
+
+const BigInteger EllipticCurve::getX() const {
+	return x;
+}
+
+void EllipticCurve::setX(const BigInteger x) {
+	this->x = x;
+}
+
+const BigInteger EllipticCurve::getY() const {
+	return y;
+}
+
+void EllipticCurve::setY(const BigInteger y) {
+	this->y = y;
+}
+
+void EllipticCurve::setA(const std::string hex) {
+	this->a.setHexValue(hex);
+}
+
+void EllipticCurve::setB(const std::string hex) {
+	this->b.setHexValue(hex);
+}
+
+void EllipticCurve::setCofactor(const std::string hex) {
+	this->cofactor.setHexValue(hex);
+}
+
+void EllipticCurve::setOrder(const std::string hex) {
+	this->order.setHexValue(hex);
+}
+
+void EllipticCurve::setP(const std::string hex) {
+	this->p.setHexValue(hex);
+}
+
+void EllipticCurve::setX(const std::string hex) {
+	this->x.setHexValue(hex);
+}
+
+void EllipticCurve::setY(const std::string hex) {
+	this->y.setHexValue(hex);
 }
