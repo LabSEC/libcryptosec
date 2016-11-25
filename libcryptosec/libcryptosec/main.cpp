2dfd987724ac24e55e9b07e1f42e1a34b6711050
Martin <gagliotti@gmail.com>
2016-11-25 15:50:28 -0200

    Initial Commit

---
+++ libcryptosec/libcryptosec/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  libcryptosec
+//
+//  Created by Martin on 25/11/16.
+//  Copyright (c) 2016 Martin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
