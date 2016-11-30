d81f2b4ed3672c7ccb7a9ce5fb8cf4a25d9c05c5
martin <gagliotti@gmail.com>
2016-11-30 18:31:18 -0200

    xx

--- include/libcryptosec/ts/TimestampResponseBuilder.h
+++ include/libcryptosec/ts/TimestampResponseBuilder.h
@@ -5,6 +5,11 @@
  *      Author: martin
  */
 
+#include <openssl/ts.h>
+
+#include <libcryptosec/ts/TimestampRequest.h>
+#include <libcryptosec/Certificate/Certificate.h>
+
 #ifndef TIMESTAMPRESPONSEBUILDER_H_
 #define TIMESTAMPRESPONSEBUILDER_H_
 
@@ -12,6 +17,25 @@ class TimestampResponseBuilder {
 public:
 	TimestampResponseBuilder();
 	virtual ~TimestampResponseBuilder();
+	void setTimestampRequest(TimestampRequest& tsreq) throw (EncodeException);
+	void setSerial(long int serial);
+	void setSignerCert(Certificate &cert);
+	TS_RESP* sign(PrivateKey &privateKey, MessageDigest::Algorithm messageDigestAlgorithm)
+	throw (AsymmetricKeyException);
+protected:
+	TS_RESP_CTX* ctx;
+	BIO* req;
+
+
 };
+
+long int serial;
+
+ASN1_INTEGER *serial_cb(TS_RESP_CTX *ctx, void *data)
+{
+	ASN1_INTEGER * asn1serial = ASN1_INTEGER_new();
+	ASN1_INTEGER_set(asn1serial, serial);
+	return asn1serial;
+}
 
 #endif /* TIMESTAMPRESPONSEBUILDER_H_ */
--- src/ts/TimestampResponseBuilder.cpp
+++ src/ts/TimestampResponseBuilder.cpp
@@ -8,11 +8,42 @@
 #include <libcryptosec/ts/TimestampResponseBuilder.h>
 
 TimestampResponseBuilder::TimestampResponseBuilder() {
-	// TODO Auto-generated constructor stub
-
+	this->ctx = TS_RESP_CTX_new();
+	this->req = NULL;
+	serial = 0;
 }
 
 TimestampResponseBuilder::~TimestampResponseBuilder() {
-	// TODO Auto-generated destructor stub
+	TS_RESP_CTX_free(this->ctx);
+	if(this->req != NULL)
+	{
+		BIO_free_all(this->req);
+	}
 }
 
+void TimestampResponseBuilder::setTimestampRequest(TimestampRequest& tsreq) throw (EncodeException)
+{
+	ByteArray der = tsreq.getDerEncoded();
+
+	if(this->req != NULL){
+		BIO_free_all(this->req);
+	}
+
+	this->req = BIO_new(BIO_s_mem());
+
+	if (this->req == NULL)
+	{
+		throw EncodeException(EncodeException::BUFFER_CREATING, "TimestampResponseBuilder::setTimestampRequest");
+	}
+
+	if ((unsigned int)(BIO_write(this->req, der.getDataPointer(), der.size())) != der.size())
+	{
+		BIO_free_all(this->req);
+		throw EncodeException(EncodeException::BUFFER_WRITING, "TimestampResponseBuilder::setTimestampRequest");
+	}
+}
+
+void TimestampResponseBuilder::setSerial(long int num) {
+	serial = num;
+	TS_RESP_CTX_set_serial_cb(this->ctx, serial_cb, NULL);
+}
